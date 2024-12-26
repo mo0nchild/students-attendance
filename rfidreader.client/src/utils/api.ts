74391c16f95fa54454ae72ba5cceef97854882f6
mo0nchild <happytown77831@gmail.com>
2024-12-27 02:29:32 +0300

    feat: config file for client

--- rfidreader.client/src/utils/api.ts
+++ rfidreader.client/src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import config from './../../configuration.json'
 
-export const apiBaseUrl = `https://192.168.0.103:8443/api`
+// export const apiBaseUrl = `https://127.0.0.1:8443/api`
+export const apiBaseUrl = `https://${config.backendIP}:${config.backendPort}/api`
 export const apiKey = '73e259e9-afb3-490b-ace1-b19c31bec8a5'
 
 const $api = axios.create({
